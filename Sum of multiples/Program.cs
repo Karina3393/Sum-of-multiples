b63f9ac2364c8eaeaeb0fa8898211540bdfb767a
карина <@DESKTOP-FP9I0KA>
2024-09-26 14:40:57 +0300

    Add project files.

---
+++ Sum of multiples/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Sum_of_multiples
+{
+    internal class Program
+    {
+        static void Main()
+        {
+           
+            Console.WriteLine("Введіть рівень:");
+            int level = int.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine("Введіть базові значення магічних предметів через кому:");
+            List<int> baseValues = Console.ReadLine().Split(',').Select(int.Parse).ToList();
+
+            HashSet<int> multiples = new HashSet<int>();
+
+            
+            foreach (int baseValue in baseValues)
+            {
+                for (int i = baseValue; i < level; i += baseValue)
+                {
+                    multiples.Add(i); 
+                }
+            }
+
+            int totalEnergyPoints = multiples.Sum();
+
+            Console.WriteLine($"Гравець заробив {totalEnergyPoints} очок енергії.");
+        }
+    }
+}
